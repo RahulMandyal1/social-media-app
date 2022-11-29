167b7c9cbd00b92a1907c72fb5dc1e432d8e7107
Rahul Thakur <rahulmandyal079@gmail.com>
2022-11-29 18:32:14 +0530

    started with the user interface

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,12 @@
+import React from "react";
+import { Button, Typography } from "@mui/material";
+import Navbar from "./components/navbar/Navbar";
+import Feed from "./components/feed/Feed";
+import RightBar from "./components/rightbar/RightBar";
+import SideBar from "./components/sidebar/SideBar";
+
 const App = () => {
-  return <> Hey user this is App component</>;
+  return <React.Fragment></React.Fragment>;
 };
 
 export default App;
---
+++ client/src/theme.tsx
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#323232",
+    },
+    secondary: {
+      main: "#edf2ff",
+    },
+  },
+});
